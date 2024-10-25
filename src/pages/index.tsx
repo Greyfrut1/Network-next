6f79b26760fc16781733454a20b37cfe40f9cde1
Greyfrut <greyfrut@wishdesk.com>
2024-10-25 11:30:00 +0300

    Base setup next

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <p>This is the index page</p>
+    </div>
+  );
+};
+
+export default IndexPage;
